249dab2770e5eefe3e58a6df5e2252e18a907bf7
Adolfo Unturbe Pérez <apu314@gmail.com>
2023-05-04 10:43:50 +0100

    03-lazy-days - starting point - update dependecies

---
+++ 03-lazy-days/client/src/components/app/hooks/useCustomToast.ts
@@ -0,0 +1,27 @@
+import {
+  // CloseAllToastsOptions,
+  ToastId,
+  ToastPosition,
+  useToast,
+  UseToastOptions,
+} from '@chakra-ui/react';
+
+type CloseAllToastsOptions = {
+  positions?: ToastPosition[];
+};
+interface UseCustomToast {
+  (options?: UseToastOptions | undefined): string | number | undefined;
+  close: (id: ToastId) => void;
+  closeAll: (options?: CloseAllToastsOptions | undefined) => void;
+  update(id: ToastId, options): void;
+  isActive: (id: ToastId) => boolean | undefined;
+}
+
+// a wrapper around Chakra UI's useToast that has my default options applied
+export function useCustomToast(): UseCustomToast {
+  return useToast({
+    isClosable: true,
+    variant: 'subtle',
+    position: 'bottom',
+  });
+}
